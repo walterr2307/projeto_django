9000be150d37b1ad122b421d7afd9d5013583835
walterr2307 <cavalcantewalter665@gmail.com>
2024-09-25 15:55:15 -0300

    imagem.html

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index)
+    path('', index, name='index'),     
+    path('imagem/', imagem, name='imagem'),  
 ]
